d9b71d122f5ab22093e607469ef1153e1475680e
Grant Kang <3s.grantkang@gmail.com>
2017-06-25 21:52:20 -0700

    Initial setup for server-side development

---
+++ server/src/main/java/com/autotownmayor/server/AutoTownMayorWebAppServerApplication.java
@@ -0,0 +1,12 @@
+package com.autotownmayor.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTownMayorWebAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTownMayorWebAppServerApplication.class, args);
+	}
+}
