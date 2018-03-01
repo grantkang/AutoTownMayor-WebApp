6674b84ed11d17a4f6a131acd882f3b0c73aa252
Grant Kang <3s.grantkang@gmail.com>
2018-03-01 02:47:52 -0800

    Client - updated footer/header components
    Footer changes:
    -tried implementing flex layout
    -now opens contact component in a dialogbox
    Header changes:
    -Now includes a route to login & logout

--- client/src/app/core/footer/footer.component.ts
+++ client/src/app/core/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { ContactComponent } from '../contact/contact.component';
 
 @Component({
   selector: 'app-footer',
@@ -8,4 +11,16 @@ import { Component } from '@angular/core';
 export class FooterComponent {
   // TODO: Fix so Ng Flex API is used properly
   copyrightMessage = '\u00A9' + new Date().getFullYear().toString() + ' AutoTownMayor';
+
+  constructor(public dialog: MatDialog) {}
+
+  onOpenContact() {
+    const dialogRef = this.dialog.open(ContactComponent, {});
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
+    });
+
+
+  }
 }
--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as fromApp from '../../store/app.reducers';
+import * as AuthAction from '../../auth/store/auth.actions';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: []
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  authState: Observable<{authenticated: boolean}>;
   title = 'Auto Town Mayor';
+
+  constructor(private store: Store<fromApp.AppState>) {}
+
+  ngOnInit() {
+    this.authState = this.store.select('auth');
+  }
+
+  onLogout() {
+    this.store.dispatch(new AuthAction.Logout());
+  }
 }
