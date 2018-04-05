f5c99b5b6e8b724cb3146e6bc30b68a338910079
Grant Kang <3s.grantkang@gmail.com>
2018-04-05 16:42:24 -0700

    Server - Created a helper method for certain REST calls
    Created a helper method to handle null Request Parameter for the ItemList REST calls.

--- server/src/main/java/com/autotownmayor/server/rest/ItemResource.java
+++ server/src/main/java/com/autotownmayor/server/rest/ItemResource.java
@@ -1,5 +1,6 @@
 package com.autotownmayor.server.rest;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import com.autotownmayor.server.converter.SalesItemResponsePriceRemover;
@@ -7,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.autotownmayor.server.converter.SalesItemEntityToSalesItemResponseConverter;
 import com.autotownmayor.server.entity.SalesItemEntity;
@@ -29,30 +27,45 @@ public class ItemResource {
 	
 	// TODO: Don't use hardcoded type String
 	@RequestMapping(path="", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public Page<SalesItemResponse> getAllItems(Pageable pageable){
-		Page<SalesItemEntity> salesItemEntityList = pageableSalesItemRepository.findByType("Inventory Part",pageable);
-		
-		return salesItemEntityList.map(new SalesItemEntityToSalesItemResponseConverter());
+	public Page<SalesItemResponse> getAllItems(Pageable pageable, @RequestParam(value="nameFilter", required=false) String nameFilter, @RequestParam(value="categoryFilter", required=false) String[] categoryFilter){
+        Page<SalesItemEntity> salesItemEntityList = salesItemEntityPageByQueryValues(ResourceConstants.DEFAULT_ITEM_TYPE, nameFilter, categoryFilter, pageable);
+
+        return salesItemEntityList.map(new SalesItemEntityToSalesItemResponseConverter());
 	}
 
     // TODO: Should just use the same URL but know whether to give prices or not depending on headers
-	@RequestMapping(path="withoutprices", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public Page<SalesItemResponse> getAllItemsWithoutPrices(Pageable pageable){
-        Page<SalesItemEntity> salesItemEntityList = pageableSalesItemRepository.findByType("Inventory Part",pageable);
+	@RequestMapping(path=ResourceConstants.WITHOUT_PRICES_PATH, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Page<SalesItemResponse> getAllItemsWithoutPrices(Pageable pageable, @RequestParam(value="nameFilter", required=false) String nameFilter, @RequestParam(value="categoryFilter", required=false) String[] categoryFilter){
+        Page<SalesItemEntity> salesItemEntityList = salesItemEntityPageByQueryValues(ResourceConstants.DEFAULT_ITEM_TYPE, nameFilter, categoryFilter, pageable);
 
         return salesItemEntityList.map(new SalesItemEntityToSalesItemResponseConverter()).map(new SalesItemResponsePriceRemover());
     }
 	
-	@RequestMapping(path="item/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(path=ResourceConstants.SINGLE_ITEM_PATH, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Optional<SalesItemResponse> getItemById(@PathVariable String id) {
 		Optional<SalesItemEntity> item = salesItemRepository.findById(id);
 		return item.map(new SalesItemEntityToSalesItemResponseConverter());
 	}
 
-    @RequestMapping(path="withoutprices/item/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(path=ResourceConstants.WITHOUT_PRICES_PATH+ResourceConstants.SINGLE_ITEM_PATH, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Optional<SalesItemResponse> getItemWithoutPriceById(@PathVariable String id) {
         Optional<SalesItemEntity> item = salesItemRepository.findById(id);
         return item.map(new SalesItemEntityToSalesItemResponseConverter()).map(new SalesItemResponsePriceRemover());
     }
 
+    // TODO: Find a better way to handle null RequestParameters
+    private Page<SalesItemEntity> salesItemEntityPageByQueryValues(String type, String nameFilter, String[] categoryFilter, Pageable pageable) {
+        if(nameFilter != null && categoryFilter != null) {
+            return pageableSalesItemRepository.findByTypeAndCategoryInAndNameContainsIgnoreCase(type, Arrays.asList(categoryFilter), nameFilter, pageable);
+        }
+        else if(nameFilter != null && categoryFilter == null) {
+            return pageableSalesItemRepository.findByTypeAndNameContainsIgnoreCase(type, nameFilter, pageable);
+        }
+        else if(nameFilter == null && categoryFilter != null) {
+            return pageableSalesItemRepository.findByTypeAndCategoryIn(type, Arrays.asList(categoryFilter), pageable);
+        }
+        else {
+            return pageableSalesItemRepository.findByType(type,pageable);
+        }
+    }
 }
--- server/src/main/java/com/autotownmayor/server/rest/ResourceConstants.java
+++ server/src/main/java/com/autotownmayor/server/rest/ResourceConstants.java
@@ -1,6 +1,12 @@
 package com.autotownmayor.server.rest;
 
+// TODO: Maybe set the values of these constants application-{environment}.properties
 public class ResourceConstants {
 	public static final String SALES_ITEM_V1 = "/items/v1";
 	public static final String CONTACT_MESSAGE_V1 = "/contact/v1";
+	public static final String SINGLE_ITEM_PATH = "item/{id}";
+	public static final String WITHOUT_PRICES_PATH = "withoutprices/";
+
+
+	public static final String DEFAULT_ITEM_TYPE = "Inventory Part";
 }
