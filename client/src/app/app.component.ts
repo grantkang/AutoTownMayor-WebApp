ec3a2dbf1f27b5a8ec6e9f57b7441e51aeb87883
Grant Kang <3s.grantkang@gmail.com>
2018-04-05 17:19:14 -0700

    Client - now checks login status to call the approriate REST endpoints

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs/Rx';
 
+import * as fromApp from './store/app.reducers';
+import * as fromAuth from './auth/store/auth.reducers';
+import * as AuthAction from './auth/store/auth.actions';
+import { Store } from '@ngrx/store';
+import { JwtHelper } from 'angular2-jwt';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnChanges {
   title = 'Auto Town Mayor';
-  isLoggedIn = false;
+  // TODO: Use angular/flex-box to style shit
+  constructor(private store: Store<fromApp.AppState>) {};
+
+  ngOnChanges() {
+    console.log('onChanges() called');
+    const token = localStorage.getItem('token');
+    const jwtHelper: JwtHelper = new JwtHelper();
+    if (jwtHelper.isTokenExpired(token)) {
+      this.store.dispatch(new AuthAction.Logout());
+    }
+  }
 }
--- client/src/app/shared/interceptor/auth.interceptor.ts
+++ client/src/app/shared/interceptor/auth.interceptor.ts
@@ -1,27 +1,38 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { AppConstant } from '../../app.constant';
+import { JwtHelper } from 'angular2-jwt';
 
+import * as fromApp from '../../store/app.reducers';
+import { Store } from '@ngrx/store';
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private store: Store<fromApp.AppState>) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', req);
-    console.log(req.params);
 
+    const jwtHelper: JwtHelper = new JwtHelper();
     const myToken = localStorage.getItem('token');
     const originalURL = req.url;
     let editedRequest;
 
-    if (myToken == null) {
+    if (myToken == null || jwtHelper.isTokenExpired(myToken)) {
       const newURL = this.convertToUnprotectedURL(originalURL);
-      console.log(newURL);
       editedRequest = req.clone({url: newURL});
     } else {
       editedRequest = this.appendAuthorizationHeader(req, myToken);
     }
-    console.log(editedRequest);
     return next.handle(editedRequest);
   }
 
+  /* TODO: Find a better way to determine which REST endpoints to use.
+   *       Should probably have the store effects decide whether to use the public/protected routes
+   *       by checking the auth state.
+   */
   convertToUnprotectedURL(originalURL: string): string {
     switch (originalURL) {
       case AppConstant.BASE_URL + AppConstant.ITEM_URL + this.getLastPathVariable(originalURL):
