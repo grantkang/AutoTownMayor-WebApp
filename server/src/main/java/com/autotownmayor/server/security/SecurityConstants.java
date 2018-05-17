a555f0a1c20d239e64f3d6a3d48355f900e3a9c9
Grant Kang <3s.grantkang@gmail.com>
2018-05-16 17:03:16 -0700

    Server - Separated Dev/Prod profiles
    TODO: Separate Security constants for dev/prod. Maybe they can be moved into the spring properties files.

--- server/src/main/java/com/autotownmayor/server/security/SecurityConstants.java
+++ server/src/main/java/com/autotownmayor/server/security/SecurityConstants.java
@@ -1,8 +1,9 @@
 package com.autotownmayor.server.security;
 
 public class SecurityConstants {
-    public static final String SECRET = "Secret";
+    public static final String SECRET = "Secret";       // TODO: Use a better secret
     public static final long TEST_EXPIRATION_TIME = 300_000; // 5 minutes
+    public static final long TEST_EXPIRATION_TIME_24HOURS = 86_400_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
