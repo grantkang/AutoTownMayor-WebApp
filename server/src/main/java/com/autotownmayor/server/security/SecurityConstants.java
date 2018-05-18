17dc261c440aa71620d29eb8699baaac544405d3
Grant Kang <3s.grantkang@gmail.com>
2018-05-18 16:26:09 -0700

    Server - Separated some constants for Dev/Prod environments

--- server/src/main/java/com/autotownmayor/server/security/SecurityConstants.java
+++ server/src/main/java/com/autotownmayor/server/security/SecurityConstants.java
@@ -1,7 +1,10 @@
 package com.autotownmayor.server.security;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SecurityConstants {
-    public static final String SECRET = "Secret";       // TODO: Use a better secret
+    @Value("${jwt.secret}")
+    public static final String SECRET = "";       // TODO: Use a better secret
     public static final long TEST_EXPIRATION_TIME = 300_000; // 5 minutes
     public static final long TEST_EXPIRATION_TIME_24HOURS = 86_400_000;
     public static final String TOKEN_PREFIX = "Bearer ";
